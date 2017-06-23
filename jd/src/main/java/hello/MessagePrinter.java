e81b000dcf5d76829336efe910b864e59106b37f
chenminghe4 <chenminghe@jd.com>
2017-06-23 15:22:37 +0800

    依赖包

---
+++ jd/src/main/java/hello/MessagePrinter.java
@@ -0,0 +1,7 @@
+package hello;
+
+/**
+ * Created by chenminghe on 2017/6/23.
+ */
+public class MessagePrinter {
+}
---
+++ jd/src/main/java/hello/MessageService.java
@@ -0,0 +1,8 @@
+package hello;
+
+/**
+ * Created by chenminghe on 2017/6/23.
+ */
+public interface MessageService {
+	String getMessage();
+}
